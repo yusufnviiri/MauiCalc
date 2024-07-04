4ed65579c0e933d57e26b5f9000b5e775bbfb7d1
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-07-04 21:53:39 +0300

    add clear button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,21 +4,38 @@
     {
         int count = 0;
         string outPutData = "0";
+        List<string> buttons = new() { "3","5"};
+        string changer = "test";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+
+        
+             private void OnAllClear(object sender, EventArgs e)
+        {
+            screen.Text = string.Empty;
+            SemanticScreenReader.Announce(screen.Text);
+        }
+
         private void OnExecuteCommand(object sender, EventArgs e)
         {
-           //screen.Text= add.Text ;
-           if (sender is Button Button)         
-            screen.Text = Button.Text;
-           
+            //screen.Text= add.Text ;
+            if (sender is Button Button)
+            {
+                foreach (string button in buttons)
+                {
+                    changer = changer + button;
+                }
 
 
-            SemanticScreenReader.Announce(screen.Text);
+                screen.Text = Button.Text + changer;
+            }
+
+
+                SemanticScreenReader.Announce(screen.Text);
         }
     }
 
