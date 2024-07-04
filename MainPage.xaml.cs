8951108ae2226bfdc0eac766d382b5924dfeb8ab
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-07-04 20:17:49 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiCalc
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnExecuteCommand(object sender, EventArgs e)
+        {
+           
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
